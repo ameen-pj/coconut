4c646737692832945d5bfce91704b161be2232d2
ameenpj <ameenjami9@gmail.com>
2024-11-23 23:41:33 +0530

    Removed RESTResource abstract class extension

--- coconut/src/main/java/com/apj/projects/coconut/StudentResource.java
+++ coconut/src/main/java/com/apj/projects/coconut/StudentResource.java
@@ -4,7 +4,6 @@ import com.apj.projects.coconut.http.enums.HTTPContentTypes;
 import com.apj.projects.coconut.resource.annotations.QueryParams;
 import com.apj.projects.coconut.resource.annotations.RequestBody;
 import com.apj.projects.coconut.resource.handlers.annotations.Path;
-import com.apj.projects.coconut.resource.rest.RESTResource;
 import com.apj.projects.coconut.resource.rest.annotations.Consumes;
 import com.apj.projects.coconut.resource.rest.annotations.GET;
 import com.apj.projects.coconut.resource.rest.annotations.POST;
@@ -12,7 +11,7 @@ import com.apj.projects.coconut.resource.rest.annotations.Produces;
 import com.apj.projects.coconut.resource.rest.annotations.RESTResourceMapping;
 
 @RESTResourceMapping("student")
-public class StudentResource extends RESTResource<Student> {
+public class StudentResource {
 
 	@Path("/")
 	@GET
@@ -22,18 +21,11 @@ public class StudentResource extends RESTResource<Student> {
 	}
 
 	@Path("/")
-	@GET
-	@Produces(HTTPContentTypes.APPLICATION_JSON)
-	public Student[] getAllStudents() {
-		return new Student[] { new Student(1, "John"), new Student(2, "Mary") };
-	}
-
-	@Path("/")
 	@POST
 	@Consumes(HTTPContentTypes.APPLICATION_JSON)
 	@Produces(HTTPContentTypes.APPLICATION_JSON)
-	public Student createStudent(@RequestBody Student student) {
-		return student;
+	public Student[] createStudent(@RequestBody Student[] students) {
+		return students;
 	}
 
 }
