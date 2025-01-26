dd136fe23dbe59a689d87c2d94103186bc17c078
ameenpj <ameenjami9@gmail.com>
2025-01-26 15:40:10 +0530

    Created Entry point for the Coconut Server

--- coconut/src/main/java/com/apj/projects/coconut/Coconut.java
+++ coconut/src/main/java/com/apj/projects/coconut/Coconut.java
@@ -3,16 +3,14 @@ package com.apj.projects.coconut;
 import com.apj.projects.coconut.concurent.ThreadpoolService;
 import com.apj.projects.coconut.server.Server;
 
-public class ServerStart {
-	public static void main(String[] args) {
+public class Coconut {
+	public static void start() {
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
 				ThreadpoolService.shutdown();
 			}
 		});
-
 		Server.start();
-
 	}
 }
